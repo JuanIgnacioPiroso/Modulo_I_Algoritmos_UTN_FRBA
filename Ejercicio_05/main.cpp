b57d7b74004d17379295b1f6d69dde6dc0c78099
JuanIgnacioPiroso <jpiroso@frba.utn.edu.ar>
2022-04-28 02:48:17 -0300

    Ejercicios Analisis de Caso

---
+++ Ejercicio_05/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int numero1;
+    int numero2;
+
+    cout << "Ingrese un numero" << endl;
+    cin >> numero1;
+
+    cout << "Ingrese otro numero " << endl;
+    cin >> numero2;
+
+    if (numero1 > numero2)
+    {
+
+        cout << numero1 << " es mayor que " << numero2 << endl;
+    }
+    else
+    {
+
+        cout << numero2 << "es mayor que " << numero1 << endl;
+    }
+
+    return 0;
+}
