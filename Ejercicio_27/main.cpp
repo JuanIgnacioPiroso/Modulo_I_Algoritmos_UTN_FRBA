0ae0832896a5101836c50bd1964b31eb262ccbb4
JuanIgnacioPiroso <jpiroso@frba.utn.edu.ar>
2022-06-10 19:07:01 -0300

    Ejercicios Integradores

--- Ejercicio_27/main.cpp
+++ Ejercicio_27/main.cpp
@@ -1,8 +1,51 @@
 #include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <string>
+#include <conio.h>
+
 using namespace std;
 
 int main()
 {
+
+    int nroEquipos = 0;
+    int codEquipo = 0;
+    int puntosTotales = 0;
+    char codResultado;
+
+    cout << "Ingrese numero de equipos " << endl;
+    cin >> nroEquipos;
+
+    for (int i = 0; i < nroEquipos; i++)
+    {
+        cout << "Ingrese codigo de equipo " << endl;
+        cin >> codEquipo;
+
+        for (int j = 0; i < nroEquipos-1; i++)
+        {
+            cout << "Ingrese codigo de resultado " << endl;
+            cin >> codResultado;
+
+            switch (codResultado)
+            {
+            case 'G':
+                puntosTotales +=3;
+                break;
+            case 'E':
+                puntosTotales++;
+
+                break;
+            
+            default:
+                break;
+            } 
+        }
+
+        cout << "Equipo: " << codEquipo << " obtuvo un total de: " << puntosTotales << " puntos " << endl;
+        puntosTotales = 0;
+        
+    }
     
 
     return 0;
---
+++ Ejercicio_27/tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+
+                puntosTotales++;
